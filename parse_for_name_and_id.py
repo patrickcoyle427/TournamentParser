fa80a7a7aa5edbe946403c6e220d06ca97cfd52f
Patrick Coyle <36138086+patrickcoyle427@users.noreply.github.com>
2018-10-30 21:57:07 -0400

    Add files via upload

--- parse_for_name_and_id.py
+++ parse_for_name_and_id.py
@@ -1,7 +1,65 @@
 #!usr/bin/python3
 
+'''
+A quick script to find and display the info the main program will
+need check for.
+'''
+
 import xml.etree.ElementTree as ET
 
-file = 'SampleFiles/Magic_Tournament_10_29_18.wer'
+# .wer file, from Wizards Event Reporter
 
-tree = ET.parse(file)
+mtg_file = 'SampleFiles/Magic_Tournament_10_29_18.wer'
+
+mtg_tree = ET.parse(mtg_file)
+
+mtg_root = mtg_tree.getroot()
+
+mtg_count = 0
+# counts done to see how many are lines are parsed.
+
+for person in mtg_root.iter('person'):
+
+    idnum = person.get('id')
+    
+    first = person.get('first')
+    middle = person.get('middle')
+    last = person.get('last')
+
+    full_name = first + middle + last
+
+    print(idnum, full_name)
+
+    mtg_count += 1
+
+print(mtg_count)
+print()
+
+# .tournament file, from the Konami Tournament Software
+
+ygo_file = 'SampleFiles/Yugioh! Sun Tournament 10-28-18 (ID E18-105063).Tournament'
+
+ygo_tree = ET.parse(ygo_file)
+
+ygo_root = ygo_tree.getroot()
+
+ygo_count = 0
+
+for player in ygo_root.iter('Player'):
+
+    idnum = player.find('ID')
+
+    if idnum != None:
+        
+        # used because the .tournament files save two different tags
+        # as <player>. This only wants the one that has the player's name
+        # and ID
+        
+        idnum = player.find('ID').text
+        firstname = player.find('FirstName').text
+        lastname = player.find('LastName').text
+
+        print(idnum, lastname, firstname)
+        ygo_count += 1
+
+print(ygo_count)
