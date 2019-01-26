cfefbd58717493e8f7bef2de70cc399477c85c91
Patrick Coyle <36138086+patrickcoyle427@users.noreply.github.com>
2019-01-26 16:17:59 -0500

    Add files via upload

--- legacy_series_parser.py
+++ legacy_series_parser.py
@@ -1,4 +1,5 @@
 #!usr/bin/python3
+
 '''
 Legacy Series Parser
 
@@ -13,19 +14,19 @@ the legacy championship
     # It is possible to see if a playoff was done in the event tag.
     # The attribute playoffstartround="n" holds this info
 
-# Figure out a way to keep updating the same spreadsheet
-
 # allow multiple file scans in a day. Find a way to scan new data without
     # adding in the same file multiple times
+    # Finish merge_old_results()
 
 import xml.etree.ElementTree as ET
 
-import csv, datetime, os.path
+import csv, datetime, os, os.path, time
 
 # ElementTree - used for parsing the .wer files, which are essentially xml files
 # csv - used for exporting the results of this script for easy reading
 # datetime - used for finding the date for use in the .csv file's filename
-# ospath - used to check for multiple versions of .csv files with the same date
+# os - used for creating folders and moving parsed files
+# os.path - used for checking for tournament and .csv files
 
 def start_parse():
 
@@ -35,19 +36,21 @@ def start_parse():
 
     if files_exist:
 
+        old_points = find_old_results()
+
         events = scan_for_events()
 
-        player_dict = parse_events(events)
+        player_dict = parse_events(events, old_points)
 
         move_files(events)
 
-        print_points(player_dict)
+        export_points(player_dict)
 
 def file_check():
 
     # will create necessary folders on first launch of program,
     # then will pass each other time. Gives a message to let the user
-    # know what to do
+    # know what to do with the created folders.
 
     if os.path.isdir('to_parse') and os.path.isdir('parsed_events'):
 
@@ -73,6 +76,8 @@ def scan_for_events():
     # to the parser to be scanned and then moved.
 
     return [file for file in os.listdir('to_parse') if file.endswith('.wer')]
+    # list comp to find all .wer files in a folder, in case other files end
+    # up in the to_parse folder.
 
 def load_root(to_parse):
 
@@ -84,9 +89,9 @@ def load_root(to_parse):
 
     return mtg_root
 
-def parse_events(event_list):
+def parse_events(event_list, old_results):
 
-    # Adds participation points for wins and draws to the player's dict
+    # Adds points for wins and draws to the player's dict
 
     # wins are determined based on match.get('outcome') return results
     # if outcome == '1' match was played normally with a winner and loser
@@ -137,6 +142,8 @@ def parse_events(event_list):
 
             elif outcome == '2':
 
+                # awards .5 points to each player in a draw
+
                 player_1_id = match.get('person')
                 
                 players_dict[player_1_id][1] += .5
@@ -144,6 +151,29 @@ def parse_events(event_list):
                 player_2_id = match.get('opponent')
                 
                 players_dict[player_2_id][1] += .5
+
+    if old_results != None:
+
+        # old_results is a list of lists containing:
+        # 0: the player's dci number
+        # 1: the player's name
+        # 2: the player's point value from before.
+
+        for player in old_results:
+
+            old_dci_num = player[0]
+            old_player_name = player[1]
+            old_player_points = float(player[2])
+
+            old_points = players_dict.get(old_dci_num)
+
+            if old_points == None:
+
+                players_dict[old_dci_num] = [old_player_name, old_player_points]
+
+            else:
+
+                players_dict[old_dci_num][1] += old_player_points
 
     return players_dict
 
@@ -184,7 +214,7 @@ def get_players(mtg_root):
         
     return player_list
 
-def print_points(players_by_id):
+def export_points(players_by_id):
 
     # Exports the points tallied
 
@@ -214,10 +244,6 @@ def print_points(players_by_id):
         # not a permenant solution
         # fix later.
 
-##    for k, v in players_by_id.items():
-##
-##        print(f'{k} ({v[0]}): {v[1]}')
-
 def move_files(event_list):
 
     # moves all files parsed to a new folder so the user knows they
@@ -227,12 +253,89 @@ def move_files(event_list):
 
         os.rename(f'to_parse/{event}', f'parsed_events/{event}')
                 
-def merge_old_results():
+def find_old_results():
 
-    # takes a previous .csv file, opens it, and merge's its points
-    # with the ones that are now being scanned
+    # Finds the .csv files that have have been created by the parser.
+    # Automatically loads the file if there is only 1 choice. Allows the user to choose
+    # the file they would like to use if there is more than one possibility.
+    
+    old_results = [file for file in os.listdir() if file.endswith('.csv') and 'Point Standings' in file]
+    # builds a list of all files that end with .csv
 
-    pass
+    if len(old_results) == 1:
+
+        # if only 1 file is found, only that file is loaded
+
+        return load_old_results(old_results[0])
+        # returns the file name to be loaded by the load_old_results function
+
+    elif len(old_results) > 1:
+
+        print('Multiple result files detected. Please choose the results you would like to build upon.')
+        # User choice to chose the .csv file they want to add to
+
+        for index, result in enumerate(old_results):
+
+            print(f'{index+1}. {result}')
+            # prints all the results found
+
+        load = False
+
+        while load == False:
+
+            try:
+                
+                choice = input('Please type the number of the file you wish to use: ')
+
+                if int(choice) > len(old_results) or int(choice) < 1:
+
+                    print(f'Not a valid choice. Please choose a number between 1 and {len(old_results)}')
+
+                else:
+
+                    print(f'File to be loaded: {old_results[int(choice)-1]}')
+                    # Check to ensure the user is loading the correct file.
+
+                    yn_choice = input('Is this correct? (y/n): ')
+                    
+                    if yn_choice.lower() in ['y', 'yes', 'yep']:
+
+                        # any other choice besides yes will let you choose another file
+
+                        return load_old_results(old_results[int(choice)-1])
+                        # returns the file name to be loaded by the load_old_results function
+
+                        #load = True
+                        # breaks out of the load loop
+
+
+            except ValueError:
+
+                print('Please choose a number.')
+
+    else:
+
+        return None
+        # Returns None if no old results are found so that the parse_events function can skip
+        # adding anything.
+
+
+def load_old_results(to_load):
+
+    # loads the old results into the parser to be added to the most recent scan.
+    # returns a list of lists to be added into the points dict
+
+    loaded_results = []
+
+    with open(to_load, newline='') as csvfile:
+
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+
+            loaded_results.append([row['DCINumber'], row['Name'], row['Points']])
+
+    return loaded_results
 
 if __name__ == '__main__':
 
